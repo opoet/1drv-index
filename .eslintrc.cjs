c16b0b8f0aef96d967cc077ac9711c851242ac57
opoet <opoet@qq.com>
2023-09-01 17:13:28 +0800

    feat: init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  "extends": [
+    "next/core-web-vitals",
+    "plugin:tailwindcss/recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended"
+  ],
+}
